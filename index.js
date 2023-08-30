9671b8156be2ccdd3fbc92db36d7354eee5cc002
KonnyGuo <guokonny@gmail.com>
2023-08-30 19:16:58 -0400

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+$("document").ready(function(){
+    $("h1").addClass("big-title marginAdd");
+    $("h1").text("bye");
+    $("button").text("never click");
+    $("h1").fadeOut().fadeIn();
+});
+
+
